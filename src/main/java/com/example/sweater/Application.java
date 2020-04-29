8885dffb497f811b714cd9a4349af9ececb9b552
Ascomos21 <kucherjavyj228@gmail.com>
2020-04-29 22:06:19 +0300

    commit

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -3,6 +3,7 @@ package com.example.sweater;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//9Hokage
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,25 +1,43 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Message {
+public class    Message {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String text;
     private String tag;
 
+    private String filename;
+
+
+    @ManyToOne (fetch = FetchType.EAGER)
+    @JoinColumn (name = "user_id")
+    private User author;
+
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author !=null ? author.getUsername():"<none>";
     }
 
     public void setText(String text) {
@@ -45,4 +63,13 @@ public class Message {
     public void setTag(String tag) {
         this.tag = tag;
     }
+
+    public String getFilename(){
+        return filename;
+    }
+
+    public void setFilename( String filename){
+        this.filename = filename;
+    }
+
 }
